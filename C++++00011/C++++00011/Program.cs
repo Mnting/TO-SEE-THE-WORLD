1d8f811f5f5e67a867dedd45689875bd0d57471e
Mnting <31748932+Mnting@users.noreply.github.com>
2017-09-14 21:15:37 +0800

    C#迭代法

--- C++++00011/C++++00011/Program.cs
+++ C++++00011/C++++00011/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+
 namespace C____001
 {
     class Program
@@ -14,60 +16,74 @@ namespace C____001
             {
                 Console.Title = "C#第一课迭代法";//设置标题
                 Console.ForegroundColor = ConsoleColor.Green; //设置字体颜色为绿色 
-                Console.Write("请输入循环次数：");
-                int n = Convert.ToInt16(Console.ReadLine());
-                Fibanacci(n);
+                Console.Write("请输入循环精度：");
+                double n = Convert.ToDouble(Console.ReadLine());
+                Fibanacci(n);           
                 Tylor(n);
                 Vieta(n);
                 Console.Write("是否继续？Y or N?");
                 string num = Console.ReadLine();
-                if ( num == "N" || num == "n")
+                if (num == "N" || num == "n")
                     flag = false;
-                Console.ReadKey();
             }
         }
 
-        static void Fibanacci(int n)
+
+
+        static void Fibanacci(double n)
         {
+            int num = n.ToString().Length;//迭代精度的字符串长度作为斐波那契的第几项
             Int64 fibanacci1 = 1, fibanacci = 1;
-            if (n == 1 || n == 2)
-                Console.Write("Fibanacci:{0}", 1);
+            if (num == 1 || num == 2)
+                Console.WriteLine("Fibanacci:{0}", 1);
             else
             {
-                for (int i = 2; i < n; i++)
+                for (int i = 2; i < num; i++)
                 {
                     fibanacci = fibanacci + fibanacci1;
                     fibanacci1 = fibanacci - fibanacci1;
                 }
-                Console.WriteLine("Fibanacci:{0}", fibanacci);
+                Console.WriteLine("Fibanacci第{0}项:{1}", num,fibanacci);
             }
         }
 
-        static void Vieta(int n)
+
+
+        static void Vieta(double n)
         {
+            int times = 0;
             double num = 1, num1 = 0;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 num1 = Math.Sqrt(num1 + 2.0);
                 num = num * num1 * 0.5;
+                times++;
+                if (Math.Abs(2.0 / num - 2.0 / num1) <= n)
+                    i = 10001;
             }
             num = 2.0 / num;
-            Console.WriteLine("π:{0}", num);
+
+            Console.WriteLine("π:{0}    times:{1}", num,times);
         }
 
-        static void Tylor(int n)
+
+
+        static void Tylor(double n)
         {
+            int times = 0;
             double e = 2, num = 1;
-            for (int i = 2; i <= n; i++)
+            for (int i = 2; i <= 10000; i++)
             {
                 for (int j = 1; j <= i; j++)
                 {
-                    num = j * num;
+                    num = j * num;times++;
                 }
                 e = e + 1 / num;
-                num = 1;
+                if (Math.Abs(1 / num) <= n)
+                    i = 10001;
+                    num = 1;
             }
-            Console.WriteLine("e:{0}", e);
+            Console.WriteLine("e:{0}    times:{1}", (e - 1/num),times);
         }
     }
 }
